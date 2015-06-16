4fd0c0e336f6601ebfa94356ddd9e1ea1b10dac2
Nguyen Khanh Linh <linh.nguyenkhanh@gmo-media.jp>
2015-06-17 07:32:31 +0900

    Create fragment example

---
+++ FrragmentSample/app/src/main/java/com/example/gmomedia/frragmentsample/Ipsum.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.gmomedia.frragmentsample;
+
+public class Ipsum {
+
+    static String[] Headlines = {
+        "Article One",
+        "Article Two",
+            "Article Three"
+
+    };
+
+    static String[] Articles = {
+        "Article One\n\nBài báo 1",
+        "Article Two\n\nBài báo 2",
+            "Article Three\n\nBài báo 3"
+    };
+}
