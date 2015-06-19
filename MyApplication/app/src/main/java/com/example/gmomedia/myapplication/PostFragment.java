3572fb93af6c7c05bd174bd89a903bc1ecddc6fb
Nguyen Khanh Linh <linh.nguyenkhanh@gmo-media.jp>
2015-06-19 18:58:37 +0900

    Practice on TechCamp ( 2015_6_18 ) Post Function implemented

--- MyApplication/app/src/main/java/com/example/gmomedia/myapplication/PostFragment.java
+++ MyApplication/app/src/main/java/com/example/gmomedia/myapplication/PostFragment.java
@@ -10,11 +10,26 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by usr0200475 on 15/06/19.
  */
-public class PostFragment extends Fragment implements View.OnClickListener{
+public class PostFragment extends Fragment implements View.OnClickListener, Response.ErrorListener, Response.Listener<String>{
+
+    RequestQueue queue;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -25,19 +40,71 @@ public class PostFragment extends Fragment implements View.OnClickListener{
 
         b.setOnClickListener(this);
 
+        queue = Volley.newRequestQueue(getActivity());
+
         return v;
     }
 
     @Override
     public void onClick(View v) {
+        TextView tv_sender = (TextView) getActivity().findViewById(R.id.sender);
+        TextView tv_message = (TextView) getActivity().findViewById(R.id.message);
+        CheckBox cb_active = (CheckBox) getActivity().findViewById(R.id.active);
 
-        TextView tv_sender = (TextView)getActivity().findViewById(R.id.sender);
-        TextView tv_message = (TextView)getActivity().findViewById(R.id.message);
-        CheckBox cb_active = (CheckBox)getActivity().findViewById(R.id.active);
+        final String sender = tv_sender.getText().toString();
+        final String message = tv_message.getText().toString();
+        final boolean activate = cb_active.isChecked();
 
-        Log.d("post", tv_sender.getText().toString());
-        Log.d("post", tv_message.getText().toString());
-        Log.d("active", Boolean.toString(cb_active.isChecked()));
+        if(sender.length() == 0){
+            Toast.makeText(getActivity(), "名前が入力されていません", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(message.length()==0){
+            Toast.makeText(getActivity(), "メッセージが入力されていません", Toast.LENGTH_LONG).show();
+            return;
+        }
 
+        String url = "http://157.7.122.113/posts/add";
+
+        StringRequest req = new StringRequest(Request.Method.POST, url, this, this){
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                HashMap<String,String>hm = new HashMap<>();
+                hm.put("data[Post][thread_id]","1");
+                hm.put("data[Post][name]", sender);
+                hm.put("data[Post][post]",message);
+                hm.put("data[Post][active]",(activate)?"1":"0");
+
+                return hm;
+            }
+        };
+        queue.add(req);
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError volleyError) {
+        Log.e("volley", volleyError.getMessage());
+        volleyError.printStackTrace();
+        getActivity().finish();
+    }
+
+    @Override
+    public void onResponse(String s) {
+        Log.d("volley", "Response : " + s);
+        getActivity().finish();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        queue.stop();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        queue.start();
     }
 }
